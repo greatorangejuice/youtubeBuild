b30ebdcd21da8a768a9f43575a6cf88ffa18a91d
greatorangejuice <bypavelsnigirev@gmail.com>
2019-04-25 14:02:21 +0300

    fix: fix notification module and structure

--- YouTube client/notification.js
+++ YouTube client/notification.js
@@ -1,23 +1,22 @@
 export function getNotification() {
+    let removeButton = document.querySelector("#remove-button");
+    let notification = document.querySelector("#notification");
+    let checkBox = document.querySelector("#disable-checkbox");
+
     if (localStorage["checked"] == "false" || !localStorage["checked"]) {
         setTimeout(() => notification.classList.remove("hide"), 1000);
     }
 
-    
-let removeButton = document.querySelector("#remove-button")[0];
-let notification = document.querySelector("#notification");
-let checkBox = document.querySelector("#disable-checkbox");
+    checkBox.addEventListener("change", () => {
+        if (checkBox.checked) {
+            localStorage.setItem("checked", "true")
+        } else {
+            localStorage.removeItem("checked");
+        }
 
-checkBox.addEventListener("change", () => {
-    if (checkBox.checked) {
-        localStorage.setItem("checked", "true") 
-    } else {
-        localStorage.removeItem("checked");
-    }
-    
-})
+    })
 
-removeButton.addEventListener("click", function() {
-    this.parentNode.classList.add("hide");
-})
+    removeButton.addEventListener("click", function () {
+        this.parentNode.classList.add("hide");
+    })
 }
