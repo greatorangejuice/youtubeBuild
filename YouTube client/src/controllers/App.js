9108ecd29f5546b1910e30c48dffd7a5335eaa5e
greatorangejuice <bypavelsnigirev@gmail.com>
2019-05-04 15:55:59 +0300

    feat: add css

--- YouTube client/src/controllers/App.js
+++ YouTube client/src/controllers/App.js
@@ -21,3 +21,4 @@ export default class App {
     view.render();
   }
 }
+// Как привязать на кнопку функцию?
--- YouTube client/src/views/views/InitialView.js
+++ YouTube client/src/views/views/InitialView.js
@@ -1,9 +1,13 @@
+import './initial-view.css';
+
 export default class InitialView {
   static getInitialDom() {
     const searchField = document.createElement('input');
     searchField.value = 'I love RSS';
+    searchField.className = 'search-field';
     const startButton = document.createElement('button');
     startButton.innerText = 'Search';
+    startButton.className = 'search-button';
     document.body.appendChild(searchField);
     document.body.appendChild(startButton);
   }
