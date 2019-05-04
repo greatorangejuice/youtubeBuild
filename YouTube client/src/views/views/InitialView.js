a0e0ad21425a23c37928fa413c3a763b49de00ed
greatorangejuice <bypavelsnigirev@gmail.com>
2019-05-04 14:02:49 +0300

    feat: add button

--- YouTube client/src/views/views/InitialView.js
+++ YouTube client/src/views/views/InitialView.js
@@ -2,6 +2,9 @@ export default class InitialView {
   static getInitialDom() {
     const searchField = document.createElement('input');
     searchField.value = 'I love RSS';
+    const startButton = document.createElement('button');
+    startButton.innerText = 'Search';
     document.body.appendChild(searchField);
+    document.body.appendChild(startButton);
   }
 }
